8e5702702f34ddf3a1d45fc02141acc9bf868c5c
Bhagyashree <167449096+PBhagyashriM@users.noreply.github.com>
2024-04-30 20:06:13 +0530

    saved file

--- src/test/java/Demo_jenkins/day_1.java
+++ src/test/java/Demo_jenkins/day_1.java
@@ -7,5 +7,23 @@ public class day_1 {
 	@Test
 	public void hi() {
 		System.out.println("hello jenkins");
+		
+		
 	}
+
+	@Test(groups= {"sanity","smoke"})
+	  public void testcase1() {
+		  System.out.println("i am in tc sanity ,smoke");
+	  }
+
+	@Test(groups= {"sanity","regression"})
+	  public void testcase4() throws InterruptedException {
+		  System.out.println("i am in tc sanity,regression");
+		  Thread.sleep(3000);
+	  }
+	@Test(groups= {"sanity"})
+	  public void testcase5() throws InterruptedException {
+		  System.out.println("i am in tc sanity");
+		  Thread.sleep(3000);
+	  }
 }
